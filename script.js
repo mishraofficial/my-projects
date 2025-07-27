d2805cb45f994ffeb7bf7dfb52e844899c9d09cf
mishraofficial <mishraofficial46@gmail.com>
2025-07-27 14:49:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap.to('#maitatti',{
+    scale:1.1,
+    from:0.5,
+    delay:1,
+    backgroundColor:"#fbafafff", borderRadius:15, delay:2,
+    duration:1,
+})
+
+gsap.to('#faltu',{
+    scale:0,from:2,
+    
+    delay:1,
+    opacity:1,
+    fromOpacity:0,
+    duration:1,
+   
+    
+})
+
