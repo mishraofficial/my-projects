d12a5315193296ef0bf7a1d2214551854c910940
mishraofficial <mishraofficial46@gmail.com>
2025-07-28 21:13:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,84 @@
-gsap.to('#maitatti',{
-    scale:1.1,
-    from:0.5,
-    delay:1,
-    backgroundColor:"#fbafafff", borderRadius:15, delay:2,
-    duration:1,
+gsap. to ('#r',{
+scale:1.1,
+duration:1,
+delay:1,
+
+
+
+})
+gsap. to ('#w',{
+scale:0, from:2,
+duration:1,
+delay:1,
+
+
 })
 
-gsap.to('#faltu',{
-    scale:0,from:2,
-    
-    delay:1,
-    opacity:1,
-    fromOpacity:0,
-    duration:1,
-   
-    
+
+gsap. to ('#q',{
+scale:0, from:2,
+duration:1,
+delay:1,
+
+
 })
 
+
+gsap. to ('#e',{
+scale:0, from:2,
+duration:1,
+delay:1,
+
+
+})
+gsap. to ('#y',{
+x:2000,
+duration:3,
+delay:1.2,
+
+
+})
+gsap. to ('#r',{
+scale:0, from:2,
+duration:1,
+delay:2,
+
+
+})
+gsap. to ('#z',{
+scale:0,
+
+
+duration:0,
+delay:0,
+
+
+})
+gsap. to ('#z',{
+scale:2, from:0,
+
+
+duration:2,
+delay:2.1,
+
+
+})
+gsap. to ('#z',{
+x:550,
+y:-300,
+
+
+
+duration:2,
+delay:2.1,
+
+
+})
+gsap. to ('#navi',{
+
+ backgroundColor:"#f5e4c3ff",
+ duration:2,
+delay:2.1,
+
+
+})
